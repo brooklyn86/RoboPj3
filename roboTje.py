cfb61fe39a0b7fcf91d3063be45170be164d356e
Victor Hugo <vbarbosa@luxfacta.com>
2020-01-10 05:34:28 -0200

    Criando metodos de captura de dados de processos no Tje

--- roboTje.py
+++ roboTje.py
@@ -4,21 +4,40 @@ from operator import index
 import pytest
 import time
 import json
+import base64
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+# Importar a classe que contém as funções e aplicar um alias
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.action_chains import ActionChains
 import requests
+import urllib.parse
 
-class RoboPje():
-    def index():
-        error = 0
-        driver = webdriver.Firefox()
+def resert():
+    index()
+
+def deleteProcesso(id, false=None):
+    print(id)
+    getProcessos = requests.get('https://fairconsultoria.com.br/v1/process/destroy/%i' % (id))
+    if(getProcessos.status_code == 200):
+        message = getProcessos.json()
+        print(message['message'])
+    else:
+        print('Error: => Erro ao deletar o processo')
+
+def index():
+    error = 0
+    driver = webdriver.PhantomJS()
+    wait = WebDriverWait(driver, 5)
+    actions = ActionChains(driver)
+    try:
         driver.get("https://esaj.tjsp.jus.br/sajcas/login")
-        driver.set_window_size(1050, 748)
         driver.find_element(By.ID, "usernameForm").click()
         driver.find_element(By.ID, "usernameForm").send_keys("967.933.923-87")
         driver.find_element(By.ID, "passwordForm").click()
@@ -28,16 +47,74 @@ class RoboPje():
                             ".esajTabelaServico:nth-child(5) .esajCelulaDescricaoServicos > a").click()
         driver.find_element(By.CSS_SELECTOR,
                             ".esajTabelaServico:nth-child(3) .esajCelulaDescricaoServicos > a").click()
-        getProcessos = requests.get('https://github.com/timeline.json')
-        if(getProcessos.status_code == 200):
-            processosCapturados = getProcessos.json()
+        print("Aguardando Resposta do Servidor com os Processos!")
+    except:
+        print('Erro: => Error de Conexão')
+        driver.quit()
+        index()
+    getProcessos = requests.get('https://fairconsultoria.com.br/v1/process')
+    if(getProcessos.status_code == 200):
+        print("Capturados!")
+        processosCapturados = getProcessos.json()
+        for processos in processosCapturados["data"]:
+            novaJanela = 0
+            print("Capturando Informaçãoes do Processo:"+ processos['processo_de_origem'])
+            removeEspacos = processos['processo_de_origem'].strip()
+            numeroProcesso = removeEspacos.split('.')
             driver.find_element(By.ID, "numeroDigitoAnoUnificado").click()
+            driver.find_element(By.ID, "numeroDigitoAnoUnificado").send_keys(numeroProcesso[0]+"."+numeroProcesso[1])
+            driver.find_element(By.ID, "foroNumeroUnificado").click()
+            foro = numeroProcesso[4].split("/")
+            driver.find_element(By.ID, "foroNumeroUnificado").send_keys(foro[0])
             driver.find_element(By.ID, "pbEnviar").click()
-        else:
-            print('Erro ao capturar os processos do servidor')
-            print('Tentando novamente')
-            error =+ 1
-            if(error > 3):
-                time.sleep(60)
-                index()
-    index()
+            try:
+                try:
+                    driver.find_element_by_partial_link_text("Precatório - 0"+foro[1]).click()
+                    driver.find_element(By.ID,"linkPasta").click()
+                    novaJanela = 1
+                    driver.switch_to.window(driver.window_handles[1])
+                    time.sleep(3)
+                    urlAtual = driver.current_url
+                    print(urlAtual)
+                    capturarCdProcesso = urlAtual.split("cdProcesso=")
+                    cdProcesso = capturarCdProcesso[1].split("&")
+                    driver.find_element(By.LINK_TEXT,"Termo de Declaração").click()
+                    time.sleep(3)
+                    urlPdf =  urllib.parse.unquote(driver.find_element(By.ID,"documento").get_property('src'))
+                    print(urlPdf)
+                    novoUrlPdf = urlPdf.split("getPDF.do?")
+                    codUrl = base64.b64encode(novoUrlPdf[1].encode('utf-8'))
+                    driver.get('https://fairconsultoria.com.br/v1/process/update/%i/%s/%s' % (processos["id"],cdProcesso[0],codUrl.decode('utf-8')))
+                    time.sleep(5)
+                    print("Processo Atualizado")
+                    driver.close()
+                    driver.switch_to.window(driver.window_handles[-1])
+                except:
+                    print("Error: => Precatoria não encontrada")
+                    deleteProcesso(processos["id"])
+                    if(novaJanela == 1):
+                        driver.close()
+                        time.sleep(3)
+                        driver.switch_to.window(driver.window_handles[-1])
+                time.sleep(2)
+            except:
+                wait.until(EC.presence_of_element_located((By.ID, 'senhaProcesso')))
+                print("Error: => Processo Não Valido")
+                deleteProcesso(processos["id"])
+                actions.key_down(Keys.ESCAPE)
+                actions.key_up(Keys.ESCAPE)
+                actions.perform()
+            time.sleep(3)
+            driver.find_element(By.ID, "numeroDigitoAnoUnificado").clear()
+            driver.find_element(By.ID, "foroNumeroUnificado").clear()
+        time.sleep(120)
+        resert()
+    else:
+        print('Error: => Erro ao capturar os processos do servidor')
+        print('Tentando novamente')
+        error =+ 1
+        if(error > 3):
+            time.sleep(60)
+            resert()
+
+index()
